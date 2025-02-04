32f333f97facf8c02a9ffdfece0189ac09c16d0f
giada <giada.beccari@joob.srl>
2025-02-04 19:13:09 +0100

    mega refactoring

--- utils/conversion_utils.c
+++ utils/conversion_utils.c
@@ -2,10 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // Boolean Conversion
 void boolean_to_string(char *buffer, size_t size, void *state) {
-    snprintf(buffer, size, "%s", (*(int *)state) ? "true" : "false");
+    bool value = *(bool *)state;
+    printf("Value: %d\n", value);
+    snprintf(buffer, size, "%s", value ? "true" : "false");
+    printf("Buffer: %s\n", buffer);
+    //snprintf(buffer, size, "%s", (*(int *)state) ? "true" : "false");
 }
 
 void boolean_update_state(const char *payload, void *state) {
